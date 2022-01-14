0dea6bfe9812a316cbd6d1ef9ba583d7972d5e50
Manisha Verma <mverma@athenasoft.co.com>
2022-01-14 12:31:17 -0800

    22

---
+++ basic/22/app.js
@@ -0,0 +1,12 @@
+//  Write a JavaScript program to remove a character at the specified position of a given string and return the new string
+function removeChar(){
+    var x=document.getElementById("enterText").value;
+console.log("value of x",x);
+var y=x.replaceAll('a',"");
+console.log('y',y);
+if(y)
+{
+    document.getElementById('containData').innerHTML=y;
+}
+}
+document.getElementById("button").addEventListener('click',removeChar);
