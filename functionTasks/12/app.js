0c529a4d52ff1835b0ac4819b5211f733300a700
manishaverma15 <manishaverma15873@gmail.com>
2022-01-23 21:23:08 +0530

    12 question

---
+++ functionTasks/12/app.js
@@ -0,0 +1,10 @@
+//  Write a function for searching JavaScript arrays with a binary search. 
+function binarySearch(){
+    var input=document.getElementsByName('array[]') ;
+    console.log("value of  sorted array",input);
+// for(i=0;i<input.length;i++){
+//     var a =input[i];
+//     console.log("value of a",a);
+// }
+}
+document.getElementById("button").addEventListener('click',binarySearch);
