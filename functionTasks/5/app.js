c913abf46ad5bf4f53de5a33ca783e269932791b
manishaverma15 <manishaverma15873@gmail.com>
2022-01-23 11:24:59 +0530

    5 question

---
+++ functionTasks/5/app.js
@@ -0,0 +1,21 @@
+// Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
+function longestCharacter(){
+    x=document.getElementById("enterString").value 
+    console.log("you entered string",x);
+    var arr = x.match(/\w[a-z]{0,}/gi);
+    console.log("array",arr);
+    var result =arr[0];
+    console.log("result",result)
+    for (i=1;i<arr.length;i++){
+        if(result.length<arr[i].length){
+            result=arr[i];
+            console.log("result",result);
+             document.getElementById("longString").innerHTML=result;
+        }
+        else{
+            document.getElementById("longString").innerHTML=result;
+
+        }
+    }
+}
+document.getElementById("button").addEventListener('click',longestCharacter);
