97f0b573f0117a41fe44c5e1fff4ed8330deb53d
manishaverma15 <manishaverma15873@gmail.com>
2022-01-29 12:59:21 +0530

    calculator

--- Assignments/1-calculator/index.js
+++ Assignments/1-calculator/index.js
@@ -8,3 +8,32 @@ functions
 class
 
 */
+class data {
+  constructor(num1, num2, operator) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.operator = operator;
+  }
+
+ calculate(){
+    switch (this.operator) {
+      case "add":
+        return this.num1 + this.num2;
+        break;
+      case "sub":
+        return this.num1 - this.num2;
+        break;
+      case "multi":
+        return this.num1 * this.num2;
+        break;
+      case "divide":
+        return this.num1 / this.num2;
+        break;
+        default:
+            return 'details is not provided'
+    }
+  }
+}
+
+  let result=new data(2,3,'add');
+console.log("result",result);
