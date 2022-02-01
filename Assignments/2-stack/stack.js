48b9a881ccb40dea877fe3dec001c008a90709fc
manishaverma15 <manishaverma15873@gmail.com>
2022-02-01 23:04:14 +0530

    pop operation

--- Assignments/2-stack/stack.js
+++ Assignments/2-stack/stack.js
@@ -1,6 +1,5 @@
 /* 
 implement fixed size stack using array
-
 const stack = new Stack(5);
 
 function to implement:
@@ -12,23 +11,38 @@ getStack - return entire stack
 
 */
 class Stack {
-    stack = [];
-    constructor(size) {
-        this.size = size;
-    }
+  stack = [];
+  constructor(size) {
+    this.size = size;
+  }
 
-    pushElement(ele) {
-        if (this.stack.length === this.size) {
-            throw Error("stack is full")
-        }
-        else {
-            this.stack[this.stack.length] = ele;
-        }
+  pushElement(ele) {
+    if (this.stack.length === this.size) {
+      throw Error("stack is full");
+    } else {
+      this.stack[this.stack.length] = ele;
+      console.log("elements:", this.stack);
     }
+  }
 
+  popElement() {
+    if (this.stack.length === 0) {
+      throw Error("stack is empty");
+    } else {
+     var lastItem = this.stack[this.stack.length - 1]
+     console.log("lastItem",lastItem);
+      this.stack.length = this.stack.length - 1;
+      console.log("after popping:", this.stack);
+return lastItem;
+    }
+  }
 }
-// const stack = new Stack(5);
-// stack.pushElement(4);
+const stack = new Stack(5);
+stack.pushElement(4);
+stack.pushElement(9);
+stack.pushElement(6);
+
+stack.popElement();
 
 module.exports = Stack;
 
--- Assignments/2-stack/stack.spec.js
+++ Assignments/2-stack/stack.spec.js
@@ -1,15 +1,31 @@
-const Stack = require ('./stack')
-describe("Testing Push",()=>{
-    test("Should throw size error", () => {
-        const stack = new Stack(0);
-        expect(() => stack.pushElement(5)).toThrow("stack is full");
-    });
+const Stack = require("./stack");
+describe("Testing Push", () => {
+  test("Should throw size error", () => {
+    const stack = new Stack(0);
+    expect(() => stack.pushElement(5)).toThrow("stack is full");
+  });
 
-    test("Should push element successfully", () => {
-        const stack = new Stack(5);
-        stack.pushElement(1);
-        stack.pushElement(2);
-        expect(stack.stack).toEqual([1, 2]);
+  test("Should push element successfully", () => {
+    const stack = new Stack(5);
+    stack.pushElement(4);
+    stack.pushElement(9);
+    stack.pushElement(6);
+    expect(stack.stack).toEqual([4, 9, 6]);
+  });
+
+  describe("Testing Pop", () => {
+    test("Should throw size error", () => {
+      const stack = new Stack(5);
+      expect(() => stack.popElement()).toThrow("stack is empty");
     });
-})
-    
+    test("Should pop element successfully", () => {
+      const stack = new Stack(5);
+      stack.pushElement(4);
+      stack.pushElement(9);
+      stack.pushElement(6);
+      expect(stack.popElement()).toBe(6)
+      expect(stack.stack).toEqual([4,9]);
+
+    });
+  });
+});
