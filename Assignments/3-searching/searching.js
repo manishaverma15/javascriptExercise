f064560d0657e8d3beb003e5e84cdf5067f475fc
Manisha Verma <mverma@athenasoft.co.com>
2022-02-03 18:20:35 -0800

    searching operation

--- Assignments/3-searching/searching.js
+++ Assignments/3-searching/searching.js
@@ -1,14 +1,16 @@
+
 class Search {
   array = [];
-  constructor(size) {
+  constructor(size, arr = []) {
     this.size = size;
+    this.array = arr;
   }
   pushElement(ele) {
     if (this.array.length === this.size) {
       throw Error("array is full");
     } else {
       this.array[this.array.length] = ele;
-      console.log("elements:", this.array);
+      // console.log("elements:", this.array);
     }
   }
 
@@ -17,23 +19,45 @@ class Search {
       throw Error("array is empty");
     }
     for (let i = 0; i <= this.array.length; i++) {
-      if (this.array[search] === this.array[this.array.length]) {
-        let index = this.array.indexOf(this.array[this.array.length]);
-        console.log("element found at :", index);
+      if (search === this.array[i]) {
+
+        return i;
       }
-      else{
-          console.log("element not found")
-      }
+
     }
 
   }
+  binarySearch(searchele) {
+    let start = 0, end = this.array.length - 1;
+    if (this.array.length === 0) {
+      throw Error("array is empty");
+    }
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      console.log("value of mid", mid);
+      if (this.array[mid] === searchele) {
+        console.log("element found at:", mid);
+        return mid;
+      }
+      else if (this.array[mid] < searchele) {
+        start = mid + 1;
+      }
+      else {
+        end = mid - 1;
+      }
+    }
+    console.log("element is  not found in the array ");
+    return -1;
+  }
 }
-const arr = new Search(10);
-arr.pushElement(8);
-arr.pushElement(78);
-arr.pushElement(34);
-arr.pushElement(17);
+// const binary = new Search(10, [5, 32, 56, 67, 88, 98]);
+//  binary.binarySearch(67);
+// const arr = new Search(10);
+// arr.pushElement(8);
+// arr.pushElement(78);
+// arr.pushElement(34);
+// arr.pushElement(17);
 
-arr.searchElement(8);
+// arr.searchElement(17);
 
 module.exports = Search;
--- Assignments/3-searching/searching.spec.js
+++ Assignments/3-searching/searching.spec.js
@@ -12,6 +12,34 @@ describe("Testing Push", () => {
     arr.pushElement(6);
     expect(arr.array).toEqual([4, 9, 6]);
   });
-  
-}
-)
+
+})
+
+
+describe("Testing Searching", () => {
+  test("Should throw size error", () => {
+    const arr = new Search(10);
+    expect(() => arr.searchElement(10)).toThrow("array is empty");
+  });
+
+  test("Should search successfully", () => {
+    const arr = new Search(10);
+    arr.pushElement(7);
+    arr.pushElement(87);
+    arr.pushElement(56);
+    arr.pushElement(12);
+    arr.pushElement(70);
+    expect(arr.searchElement(56)).toBe(2);
+  });
+})
+
+describe("Testing Binary Search", () => {
+  test("Should throw size error", () => {
+    const binaryArray = new Search(10);
+    expect(() => binaryArray.binarySearch(10).toThrow("array is empty"));
+  });
+  test("Should search successfully", () => {
+    const binary = new Search(10, [5, 32, 56, 67, 88, 98]);
+    expect(binary.binarySearch(67)).toBe(3);
+  })
+})
