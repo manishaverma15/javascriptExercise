fe22e7f4d95caad90db3515a77638e061e5d5022
manishaverma15 <manishaverma15873@gmail.com>
2022-02-04 11:10:18 +0530

    linked list operation

--- Assignments/5-linkedList/linkedList.js
+++ Assignments/5-linkedList/linkedList.js
@@ -4,63 +4,65 @@
 // removeFrom(location)
 // removeElement(element)
 
-
 // Add element
 class Node {
-    constructor(element, next = null) {
-        this.element = element;
-        this.next = next;
-    }
+  constructor(element, next = null) {
+    this.element = element;
+    this.next = next;
+  }
 }
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+  add(element) {
+    // create new node
+    let node = new Node(element);
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      console.log("current node", currentNode);
+      // repeat till last node
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
     }
-    add(element) {
-        // create new node
-        let node = new Node(element)
-        if (this.head === null) {
-            this.head = node;
-        }
-        else {
-            let currentNode = this.head;
-            console.log("current node", currentNode)
-            // repeat till last node
-            while (currentNode.next !== null) {
-                currentNode = currentNode.next;
-            }
-            currentNode.next = node;
-        }
+  }
+
+  returnArray() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode.next !== null) {
+      array.push(currentNode.element);
+      currentNode = currentNode.next;
     }
+    return array;
+  }
 
-    returnArray() {
-        let array = [];
-        let currentNode = this.head;
-        while (currentNode.next !== null) {
-            array.push(currentNode.element);
-            currentNode = currentNode.next;
+  addElementAtLocation(element, index) {
+    if (index < 0 ) {
+      return console.log("enter a valid index");
+    } else {
+      let node = new Node(element);
+      let currentNode;
+      currentNode = this.node;
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        currentNode = this.node;
+        let i = 1;
+        while (i < index) {
+          node.next == currentNode.next;
+          currentNode.next = node;
+          i++;
         }
-        return array;
+        return node;
+      }
     }
-
-     addElementAtLocation(element,index){
-         if(index <0 || index > size){
-             return console.log("enter a valid index");
-         }
-         else{
-             let node =new Node(element);
-             let currentNode,prevNode;
-             currentNode = this.node;
-             if(index === 0){
-                 node.next = this.head;
-                 this.head = node;
-             }
-             else{
-                 
-             }
-         }
-
-     }
+  }
 }
 const linkedlist = new LinkedList();
 linkedlist.add(30);
@@ -69,6 +71,8 @@ linkedlist.add(39);
 linkedlist.add(34);
 linkedlist.add(76);
 
+
+linkedlist.addElementAtLocation(45,2);
 console.log(linkedlist.returnArray());
 
 // console.log("head", linkedlist.head)
