25098f2cf4de0df48f1c3284a680ed10caabf20c
manishaverma15 <manishaverma15873@gmail.com>
2022-02-14 23:06:14 +0530

    crud-with-api

--- projects/crudWithApi/crudwithapi.js
+++ projects/crudWithApi/crudwithapi.js
@@ -2,107 +2,116 @@ let isEditing = false;
 let selectedRow = null;
 let editEle = null;
 
-
 async function getPosts() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    return response.json();
-
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return response.json();
 }
 async function deletePost(id, ele) {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: 'DELETE',
-    });
-    const data = await response.json();
-    console.log("deleteData", data);
-    ele.remove();
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  const data = await response.json();
+  console.log("deleteData", data);
+  ele.remove();
 }
-async function editPost(id, title, description,ele) {
-   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-            id: id,
-            title: title,
-            body: description,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    return  response.json();
+async function editPost(id, title, description, ele) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({
+        id: id,
+        title: title,
+        body: description,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  return response.json();
 }
 
 async function createPost(id, title, description) {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            id: id,
-            title: title,
-            body: description,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      id: id,
+      title: title,
+      body: description,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
 
-    return response.json();
-
+  return response.json();
 }
 
 async function form() {
-    let data = {};
-    data["ID"] = document.getElementById("ID").value;
-    console.log("ID:", data["ID"]);
-    data["title"] = document.getElementById("title").value;
-    console.log("title:", data["title"]);
-    data["description"] = document.getElementById("description").value;
-    console.log("description:", data["description"]);
-    if (isEditing === true) {
-        const editdata = await editPost(data["ID"], data["title"], data["description"])
-        updateRecord(editdata);
-    }
-    else {
-        const post = await createPost(data["ID"], data["title"], data["description"])
-        renderPost(post);
-    }
-    resetForm();
+  let data = {};
+  data["ID"] = document.getElementById("ID").value;
+  console.log("ID:", data["ID"]);
+  data["title"] = document.getElementById("title").value;
+  console.log("title:", data["title"]);
+  data["description"] = document.getElementById("description").value;
+  console.log("description:", data["description"]);
+  if (isEditing === true) {
+    const editdata = await editPost(
+      data["ID"],
+      data["title"],
+      data["description"]
+    );
+    updateRecord(editdata);
+  } else {
+    const post = await createPost(
+      data["ID"],
+      data["title"],
+      data["description"]
+    );
+    renderPost(post);
+  }
+  resetForm();
 }
 
 async function editData(id, title, description, ele) {
-    isEditing = true;
-    editEle = ele;
-    console.log("value-of-editele",editEle)
-    document.getElementById("ID").value = id;
-    document.getElementById("title").value = title;
-    document.getElementById("description").value = description;
-
+  isEditing = true;
+  editEle = ele;
+  console.log("value-of-editele", editEle);
+  document.getElementById("ID").value = id;
+  document.getElementById("title").value = title;
+  document.getElementById("description").value = description;
 }
 function updateRecord(data) {
-    console.log("data", data);
-    editEle.id = data.id;
-    editEle.title = data.title;
-    editEle.description = data.description;
-
-
-
+  console.log("data", data);
+  editEle.id = data.id;
+  editEle.title = data.title;
+  editEle.description = data.description;
 }
 function resetForm() {
-    document.getElementById("ID").value = "";
-    document.getElementById("title").value = "";
-    document.getElementById("description").value = "";
-
+  document.getElementById("ID").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("description").value = "";
 }
 function renderPost(todo) {
-    const ele = document.createElement('div');
-    ele.innerHTML = todoHtml(todo.id, todo.title, todo.body);
-    document.body.appendChild(ele);
-    document.getElementById(`delete-post-${todo.id}`)
-        .addEventListener("click", () => deletePost(todo.id, ele));
-    document.getElementById(`edit-post-${todo.id}`)
-        .addEventListener("click", () => editData(todo.id, todo.title, todo.body, ele));
+  const ele = document.createElement("div");
+  ele.innerHTML = todoHtml(todo.id, todo.title, todo.body);
+  document.body.appendChild(ele);
+  document
+    .getElementById(`delete-post-${todo.id}`)
+    .addEventListener("click", () => deletePost(todo.id, ele));
+  document
+    .getElementById(`edit-post-${todo.id}`)
+    .addEventListener("click", () =>
+      editData(todo.id, todo.title, todo.body, ele)
+    );
 }
 
 function todoHtml(id, title, body) {
-    return `
+  return `
         <span id="eleid">ID = ${id} </span> <br>
         <span id = "eletitle">Title = ${title} </span> <br>
         <span id = "eledescription"> Description = ${body} </span> <br>
@@ -113,22 +122,17 @@ function todoHtml(id, title, body) {
     `;
 }
 
-
 async function onClick() {
-    const todos = await getPosts();
-    console.log("array", todos)
-    for (const todo of todos) {
-        renderPost(todo);
-    }
-
+  const todos = await getPosts();
+  console.log("array", todos);
+  for (const todo of todos) {
+    renderPost(todo);
+  }
 }
-
 
 function init() {
-    document.getElementById("button").addEventListener("click", onClick);
+  document.getElementById("button").addEventListener("click", onClick);
 }
 document.getElementById("add").addEventListener("click", form);
-
-
 
 init();
